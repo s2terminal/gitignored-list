92455cdb484fae53f28830224cb6e278c0e0bf56
s2terminal <suzuki.sh@s2terminal.com>
2025-01-04 13:05:23 +0900

    use pydantic-settings

--- main.py
+++ main.py
@@ -2,14 +2,11 @@ import subprocess
 from pathlib import Path
 import pprint
 
-IGNORES = [
-    'node_modules',
-    'webpack',
-    '__pycache__',
-    '\\.next',
-    '\\.astro',
-    # '\\.venv'
-]
+import yaml
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    ignores: "list[str]"
 
 def list_git_projects(root_path: Path) -> "list[Path]":
     # 指定したパスの中にあるgit管理されたディレクトリをリストアップする
@@ -17,8 +14,8 @@ def list_git_projects(root_path: Path) -> "list[Path]":
     return []
 
 # git管理下のプロジェクトパスを受け取り、重要そうなファイルをリストする
-def list_remained_files(project_path: Path) -> "list[Path]":
-    grep_cmd = f"grep -v -e {' -e '.join(IGNORES)}"
+def list_remained_files(project_path: Path, ignores: "list[str]") -> "list[Path]":
+    grep_cmd = f"grep -v -e {' -e '.join(ignores)}"
     print(grep_cmd)
     result = subprocess.run(
         f"git ls-files --other --ignored --exclude-standard | {grep_cmd} ",
@@ -31,4 +28,6 @@ def list_remained_files(project_path: Path) -> "list[Path]":
     return [ (project_path / Path(p)).resolve() for p in path_str_list if len(p) > 0 ]
 
 if __name__ == "__main__":
-    pprint.pp(list_remained_files(Path("./")))
+    with open("config.yaml", "r") as file:
+        settings = Settings(**yaml.safe_load(file))
+    pprint.pp(list_remained_files(Path("./"), ignores=settings.ignores))
