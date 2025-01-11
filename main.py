b05e1f931d1ac549c958a49ff0c20a0799496dc0
s2terminal <suzuki.sh@s2terminal.com>
2025-01-11 17:57:30 +0900

    change relative path

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import pprint
 from typing import Tuple, Type
 
+from pydantic import BaseModel
 from pydantic_settings import (
     BaseSettings,
     PydanticBaseSettingsSource,
@@ -27,10 +28,10 @@ class Settings(BaseSettings):
     path: str
 
     def cli_cmd(self) -> None:
-        for project_path in list_git_projects(Path(self.path)):
-            print("##", project_path)
+        for project in list_git_projects(Path(self.path)):
+            print("##", project)
             pprint.pp(
-                list_remained_files(project_path, unnecessaries=self.unnecessaries)
+                list_remained_files(project, unnecessaries=self.unnecessaries)
             )
 
     # https://docs.pydantic.dev/latest/concepts/pydantic_settings/#other-settings-source
@@ -45,30 +46,52 @@ class Settings(BaseSettings):
     ) -> Tuple[PydanticBaseSettingsSource, ...]:
         return (YamlConfigSettingsSource(settings_cls),)
 
+class Project(BaseModel):
+    """
+    self.path Path:
+        コマンド実行時の解決可能なパス
+    self.name str:
+        保存先のパスとして使う名前
+    """
+    path: Path
+    name: str
+
+    def __init__(self, **data):
+        super().__init__(**data)
+        self.validate_path()
+
+    def validate_path(self):
+        if not self.path.exists():
+            raise Exception("存在しないパスです", self.path)
+
 def _run_cmd(command: str, workdir: Path) -> str:
     logger.debug(command)
     result = subprocess.run(command, shell=True, cwd=workdir, capture_output=True)
     return result.stdout.decode()
 
-def list_git_projects(root_path: Path) -> "list[Path]":
-    # 指定したパスの中にあるgit管理されたディレクトリをリストアップする
+def list_git_projects(root_path: Path) -> "list[Project]":
+    """
+    指定したパスの中にあるgit管理プロジェクトをリストアップする
+    """
     project_path_list = _run_cmd(
         'find ./ -name ".git" -type d -print0 | xargs -0 -I {} dirname {}',
         root_path
     ).split("\n")
-    # TODO: 相対パスを保持する
-    return [ (root_path / Path(p)).resolve() for p in project_path_list if len(p) > 0 ]
+    return [ Project(path=( root_path / Path(p)), name=p) for p in project_path_list if len(p) > 0 ]
 
-# git管理下のプロジェクトパスを受け取り、重要そうなファイルをリストする
-def list_remained_files(project_path: Path, unnecessaries: "list[str]") -> "list[Path]":
+def list_remained_files(project: Project, unnecessaries: "list[str]") -> "list[Path]":
+    """
+    git管理プロジェクトを受け取り、重要そうなファイルをリストする
+    プロジェクトパスからの相対パスのリストを返す
+    """
     grep_cmd = f"grep -v -e {' -e '.join(unnecessaries)}"
     path_str_list = _run_cmd(
         command=f"git ls-files --other --ignored --exclude-standard | {grep_cmd} ",
-        workdir=project_path
+        workdir=project.path
     ).split("\n")
     # TODO: シンボリックリンクでプロジェクト外を見ているのを削除する
-    abs_path_list = [ (project_path / Path(p)).resolve() for p in path_str_list if len(p) > 0 ]
-    return list(filter(lambda p: p.exists(), abs_path_list))
+    path_list = [ (project.path / Path(p)) for p in path_str_list if len(p) > 0 ]
+    return list(filter(lambda p: p.exists(), path_list))
 
 if __name__ == "__main__":
     CliApp.run(Settings)
